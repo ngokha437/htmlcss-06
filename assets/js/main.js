99d07684f3820b76b27e1860a313e82b32cc53ab
Ngo Kha <ngokha437@gmail.com>
2025-08-09 19:10:30 +0700

    handle change active stats item

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,13 @@
 import { Media } from "./media.js";
 
+document.addEventListener("DOMContentLoaded", () => {
+  Media.start();
+});
+
+// Header
 const header = $("#sticky-header");
 
-document.addEventListener("scroll", (event) => {
+document.addEventListener("scroll", () => {
   if (window.scrollY > 0) {
     header.classList.add("scrolled");
   } else {
@@ -10,6 +15,30 @@ document.addEventListener("scroll", (event) => {
   }
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  Media.start();
+// Stats
+const statsDots = $$(".stats__dot");
+const statsItems = $$(".stats__item");
+
+function setActive(index) {
+  statsDots.forEach((dot) => dot.classList.remove("stats__dot--active"));
+  statsItems.forEach((item) => item.classList.remove("stats__item--active"));
+
+  statsDots[index].classList.add("stats__dot--active");
+  statsItems[index].classList.add("stats__item--active");
+}
+
+statsDots.forEach((dot, index) => {
+  dot.addEventListener("click", function () {
+    if (!this.classList.contains("stats__dot--active")) {
+      setActive(index);
+    }
+  });
+});
+
+statsItems.forEach((item, index) => {
+  item.addEventListener("click", function () {
+    if (!this.classList.contains("stats__item--active")) {
+      setActive(index);
+    }
+  });
 });
